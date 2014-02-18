a019c488e5322ecc6a2a75a88869b9d099b5fa0d
Hovhannes Sargsyan <sarhov@sarhov.com>
2014-02-18 04:15:50 +0400

    update

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,36 @@
 jQuery(document).ready(function($) {
-	
+
+function get_four(){
+   $('.product-box').removeClass('size_6').addClass('size_3');
+    $('.product-view-type_col-four').addClass('active').siblings().removeClass('active');
+   
+}
+
+function get_two(){
+   $('.product-box').removeClass('size_3').addClass('size_6');
+    $('.product-view-type_col-two').addClass('active').siblings().removeClass('active');
+  
+}
+
+if($.cookie('view') == 'four'){ get_four(); }
+
+
+$('.product-view-type_col-four').click(function(){  
+    $.cookie('view', 'four');
+    get_four();
+    return false;
 });
+
+$('.product-view-type_col-two').click(function(){
+    $.cookie('view', 'two');
+    get_two();
+    return false;
+
+});
+
+
+});
+
+
+
+
